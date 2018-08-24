1ecfd9da7782ad6f63c6811e5374e19ba4bbe512
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-08-24 10:43:59 +0200

    first version of OpenLibrary + OldOpenLibrary implemented

--- libs/exec/openlibrary.c
+++ libs/exec/openlibrary.c
@@ -1,13 +1,34 @@
 #include <exec/types.h>
 #include <exec/libraries.h>
+#include <dlfcn.h>
 
+// Full path resolving not implemented yet
 struct Library * OpenLibrary(const char * name, uint32_t minVersion)
 {
     struct Library *retval = NULL;
+    void *handle = NULL;
+
+    // Try to open the library in lazy + local mode. Do not expose any symbols
+    handle = dlopen(name, RTLD_LAZY | RTLD_LOCAL);
+
+    if (handle) {
+        // If the library was opened, search for the weak symbol LibBase in it.
+        // The symbol points to library base with ID string and version number
+        struct Library **sym = dlsym(handle, "LibBase");
+
+        if (sym) {
+            retval = *sym;
+            if (retval->lib_Revision < minVersion) {
+                retval = NULL;
+            }
+        }
+    }
 
     return retval;
 }
 
+// Old version of OpenLibrary, keept for nostalgic reasons. It ignores the version
+// number and just opens the library
 struct Library * OldOpenLibrary(const char * name)
 {
     return OpenLibrary(name, 0);
