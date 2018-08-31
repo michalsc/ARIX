bf4fda084a024a6f39366fa91583ea2b7c174551
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-08-31 10:12:08 +0200

    Message ports use non-blocking sockets

--- libs/exec/createmsgport.c
+++ libs/exec/createmsgport.c
@@ -13,6 +13,8 @@
 
 #include "exec_intern.h"
 
+// Get "unique" uuid by reading from random number generator. Since the uuid is used for a message
+// port, set the node type in clock_seq_low.
 static uuid_t getRandomID()
 {
     union {
@@ -20,16 +22,15 @@ static uuid_t getRandomID()
         uint8_t data[16];
     } id;
 
-    printf("getRandomID()\n");
     int fd = open("/dev/random", O_RDONLY);
-    printf("fd=%d\n", fd);
     read(fd, id.data, 16);
     close(fd);
-    printf("closed\n");
+
     id.uuid.clock_seq_hi_and_reserved &= 0x0f;
     id.uuid.clock_seq_hi_and_reserved |= 0x80;
     id.uuid.time_hi_and_version &= 0x0fff;
     id.uuid.time_hi_and_version |= 0x4000;
+    id.uuid.clock_seq_low = NT_MSGPORT;
 
     return id.uuid;
 }
@@ -38,11 +39,15 @@ struct MsgPort * CreateMsgPort()
 {
     struct sockaddr_un name;
     struct MsgPort * port = AllocMem(sizeof(struct MsgPort), MEMF_CLEAR);
+
     printf("[EXEC] CreateMsgPort()\n");
 
-    if (port) {
-        printf("[EXEC] port=%p\n", port);
+    if (port)
+    {
         port->mp_Socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+        int flags = fcntl(port->mp_Socket, F_GETFL);
+        fcntl(port->mp_Socket, F_SETFL, flags | O_NONBLOCK);
 
         if (port->mp_Socket < 0)
         {
@@ -65,13 +70,10 @@ struct MsgPort * CreateMsgPort()
         int maxtry = 1000;
         int err = 0;
 
-        printf("[EXEC] Time for port id...\n");
         do {
-            printf("maxtry=%d, err=%d\n", maxtry, err);
             port->mp_ID = getRandomID();
-            printf("[EXEC] got random\n");
             CopyMem(&port->mp_ID, &name.sun_path[1], sizeof(uuid_t));
-            printf("[EXEC] Copied\n");
+            
             if (--maxtry < 0)
             {
                 printf("[EXEC] too many tries. giving up\n");
@@ -80,7 +82,7 @@ struct MsgPort * CreateMsgPort()
                 FreeMem(port, sizeof(struct MsgPort));
                 return NULL;
             }
-            printf("[EXEC] Trying to bind random port = {%08x-%04x-%04x-%04x-%02x%02x%02x%02x%02x%02x}\n",
+            printf("[EXEC] Binding to port ID {%08x-%04x-%04x-%04x-%02x%02x%02x%02x%02x%02x}\n",
                    port->mp_ID.time_low, port->mp_ID.time_med, port->mp_ID.time_hi_and_version,
                    port->mp_ID.clock_seq_hi_and_reserved << 8 | port->mp_ID.clock_seq_low,
                    port->mp_ID.node[0], port->mp_ID.node[1], port->mp_ID.node[2],
