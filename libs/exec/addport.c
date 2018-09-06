66f9e8c36b62ee5d855421c5d583e69a8f075e8d
Michal Schulz <michal.schulz@gmx.de>
2018-09-06 13:53:22 +0200

    added autodoc for AddPort

--- libs/exec/addport.c
+++ libs/exec/addport.c
@@ -26,6 +26,40 @@
 
 #define D(x) x
 
+/**
+ * NAME
+ *      AddPort - Add the message port to the list of public ports
+ * 
+ * SYNOPSIS
+ *      void AddPort(struct MsgPort *port, const char *name);
+ * 
+ * FUNCTION
+ *      Adds given message port to the list of public ports and advertise
+ *      it unter specified name. After this call any other application can
+ *      find the message port by the name and thus send messages to it.
+ * 
+ *      The message ports in ARIX are identified by type 4 (random) uuid
+ *      generated every time the CreateMsgPort function is called. The 
+ *      port IDs may vary even for the same application started twice or
+ *      in parallel. Since a random guess of a 128 bit value is rather hard,
+ *      public ports need to advertise themself by other means, here, a
+ *      name.
+ * 
+ *      Since each public port name has to be unique, the port must be 
+ *      removed form the public port list by a RemPort call as soon as 
+ *      port is deleted.
+ * 
+ * INPUTS
+ *      port - Message port to be added to the public port list.
+ *      name - A null-terminated string with public port name.
+ * 
+ * NOTE
+ *      Further versions of exec.library may remove the port automatically
+ *      upon application exit.
+ * 
+ * SEE ALSO
+ *      RemPort, CreateMsgPort(), DeleteMsgPort()
+*/
 void AddPort(struct MsgPort *port, const char * name)
 {
     uuid_t arixPort = MAKE_UUID(0x00000001, 0x0000, 0x4000, 0x8000 | NT_MSGPORT, 0x000000000000);
