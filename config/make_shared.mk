58a02e56590b96e3c94bebf2c1e0c96ce3d5fb45
Michal Schulz <michal.schulz@gmx.de>
2018-09-09 20:16:25 +0200

    add option to autolink ARIX libraries against other shared ARIX libraries

--- config/make_shared.mk
+++ config/make_shared.mk
@@ -2,6 +2,7 @@ EXTRA_CFLAGS := $(EXTRA_CFLAGS) -fpic -DVERSION_STRING_DATE='$(VERSION_STRING_DA
 EXTRA_CXXFLAGS := $(EXTRA_CXXFLAGS) -fpic -DVERSION_STRING_DATE='$(VERSION_STRING_DATE)'
 
 _M_LDLIBS := $(foreach l,$(LDLIBS),-l$(l))
+_M_AUTOLIBS := $(foreach l,$(AUTOLIBS),$(BIN_DIR)/Libs/$(l).library)
 _M_OBJDIR := $(BUILD_DIR)/$(TARGETDIR)/$(OBJNAME)
 _M_TARGETDIR := $(BIN_DIR)/$(TARGETDIR)
 _M_OBJS := $(foreach f,$(FILES),$(f).o)
@@ -31,7 +32,7 @@ build: $(addprefix $(_M_OBJDIR)/,$(_M_OBJS)) $(_M_OBJDIR)/version.script
 $(_M_TARGETDIR)/$(OBJNAME).$(OBJTYPE): $(addprefix $(_M_OBJDIR)/,$(_M_OBJS) __base_file.o) $(_M_OBJDIR)/version.script
 	@echo "* Linking $(OBJNAME).$(OBJTYPE)"
 	@$(CC) -fpic -shared -Wl,-soname,$(OBJNAME).$(OBJTYPE) -Wl,--version-script=$(_M_OBJDIR)/version.script \
-	 -fvisibility=hidden -o $@ $(foreach f,$(_M_OBJS) __base_file.o,$(_M_OBJDIR)/$(f)) $(_M_LDLIBS)
+	 -fvisibility=hidden -o $@ $(foreach f,$(_M_OBJS) __base_file.o,$(_M_OBJDIR)/$(f)) $(_M_LDLIBS) $(_M_AUTOLIBS)
 	@rm -rf $(_M_OBJDIR)/.compiled
 
 .PHONY: all
