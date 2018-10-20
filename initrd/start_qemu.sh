9354b7714f0cce33adc18118b2bc68c7b4ac0c50
Michal Schulz <michal.schulz@gmx.de>
2018-10-20 14:08:20 +0200

    start qemu in nographic mode.

--- initrd/start_qemu.sh
+++ initrd/start_qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-x86_64 -serial stdio -kernel ../Build/vmlinuz -initrd ../Build/initrd -m 512 -append "console=ttyS0 loglevel=7"
+qemu-system-x86_64 -kernel ../Build/vmlinuz -initrd ../Build/initrd -m 512 -nographic -append "console=ttyS0 loglevel=7"
