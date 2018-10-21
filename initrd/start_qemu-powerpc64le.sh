22621e6e266b6826bfda2c41e6b6886bd576f1e9
Michal Schulz <michal.schulz@gmx.de>
2018-10-21 13:34:49 +0200

    one core shall be enough for qemu

--- initrd/start_qemu-powerpc64le.sh
+++ initrd/start_qemu-powerpc64le.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-ppc64 -smp cores=2,threads=1 -nodefaults  -serial stdio -m 1024 -kernel ../Build-powerpc64le/vmlinuz -initrd ../Build-powerpc64le/initrd -nographic 
+qemu-system-ppc64 -smp cores=1,threads=1 -nodefaults  -serial stdio -m 1024 -kernel ../Build-powerpc64le/vmlinuz -initrd ../Build-powerpc64le/initrd -nographic 
