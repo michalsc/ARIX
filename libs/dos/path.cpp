e6c14d044e916317090fdc1dbfe03eb535b05816
Michal Schulz <michal.schulz@gmx.de>
2018-10-27 00:08:22 +0200

    path resolver the other way round - from UNIX to ARIX

--- libs/dos/path.cpp
+++ libs/dos/path.cpp
@@ -6,10 +6,17 @@
     Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
     with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+#include <sys/syscall.h>
 
 #include <string>
 #include <vector>
+#include <list>
 #include <algorithm>
+
+#include <fcntl.h>
+#include <dirent.h>
 
 #include "dos_private.h"
 #include "path.h"
@@ -18,9 +25,67 @@
 #define DEBUG
 #include "dos_debug.h"
 
-static std::string str_toupper(std::string s) {
+static std::string str_toupper(std::string s)
+{
     std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c){ return std::toupper(c); });
     return s;
+}
+
+static std::vector<std::string> split_to_vector(std::string s, std::size_t skip=0)
+{
+    std::vector<std::string> list;
+    std::size_t pos = 0;
+    std::size_t lastpos = skip;
+
+    do
+    {
+        pos = s.find('/', lastpos);
+        std::string elem = s.substr(lastpos, pos - lastpos);
+
+        list.push_back(elem);
+
+        lastpos = pos + 1;
+    } while (pos != std::string::npos);
+
+    return list;
+}
+
+static std::list<std::string> split_to_list(std::string s, std::size_t skip=0)
+{
+    std::list<std::string> list;
+    std::size_t pos = 0;
+    std::size_t lastpos = skip;
+
+    do
+    {
+        pos = s.find('/', lastpos);
+        std::string elem = s.substr(lastpos, pos - lastpos);
+
+        list.push_back(elem);
+
+        lastpos = pos + 1;
+    } while (pos != std::string::npos);
+
+    return list;
+}
+
+static void print(const std::vector<std::string> list)
+{
+    D(bug("print_vector\n"));
+    for (int i=0; i < list.size(); i++)
+    {
+        D(bug("element %3d: '%s'\n", i, list[i].c_str()));
+    }
+}
+
+static void print(const std::list<std::string> list)
+{
+    D(bug("print_vector\n"));
+    int i=0;
+    for (auto it: list)
+    {
+        D(bug("element %3d: '%s'\n", i++, it.c_str()));
+    }
 }
 
 Path Path::PathFromDOS(const std::string & path)
@@ -84,46 +149,34 @@ Path Path::PathFromDOS(const std::string & path)
     D(bug("[DOS] Before canonicalization: Volume=%s, Path=%s\n", p.__volume.c_str(), p.__path.c_str()));
 
     /* Canonicalize path and check if it does not try to go outside the volume */
-    std::vector<std::string> path_array;
+    std::list<std::string> path_array = split_to_list(p.__path);
     bool success = true;
     std::size_t lastpos = 0;
     int cnt = 10;
-    do
+
+    for (auto it = path_array.begin(); it != path_array.end(); ++it)
     {
-        pos = p.__path.find('/', lastpos);
-        std::string elem = p.__path.substr(lastpos, pos - lastpos);
-
-        if (elem != ".")
+        if (*it == "" || *it == "..")
         {
-            if (elem == "" || elem == "..")
-            {
-                if (path_array.size() > 0)
-                    path_array.pop_back();
-                else
-                {
-                    success = false;
-                    break;
-                }
-            }
+            if (it == path_array.begin())
+                it = path_array.erase(it);
             else
-                path_array.push_back(elem);
+            {
+                it = std::prev(it);
+                it = path_array.erase(it);
+                it = path_array.erase(it);
+            }
         }
-        lastpos = pos + 1;
-    } while (pos != std::string::npos);
-
-    if (!success) {
-        bug("Path excaping the volume boundary\n");
     }
-    else {
-        p.__path = path_array[0];
-        for (int i=1; i < path_array.size(); i++)
-        {
-            p.__path += "/" + path_array[i];
-            D(bug("element %3d: '%s'\n", i, path_array[i].c_str()));
-        }
 
-        D(bug("[DOS] canonicalized path: '%s'\n", p.__path.c_str()));
+    auto it = path_array.begin();
+    p.__path = *it;
+    for (++it; it != path_array.end(); ++it)
+    {
+        p.__path += "/" + *it;
     }
+
+    D(bug("[DOS] canonicalized path: '%s'\n", p.__path.c_str()));
 
     return p;
 }
@@ -131,6 +184,177 @@ Path Path::PathFromDOS(const std::string & path)
 Path Path::PathFromUNIX(const std::string & path)
 {
     Path p;
+
+    D(bug("[DOS] Path::PathFromUNIX(%s)\n", path.c_str()));
+
+    /* If the path is not absolute, continue with DOS path resolution */
+    if (path[0] != '/')
+    {
+        return PathFromDOS(path);
+    }
+    else
+    {
+        /* Dealing with absolute path. Check mounted Volumes if it could be any of them */
+        char linkbuf[512];
+        bool found_match = false;
+
+        /* First of all canonicalize the path. First step is to split the path into separate elements*/
+        std::list<std::string> path_array = split_to_list(path, 1);
+        print(path_array);
+        bool restart_last = false;
+        bool restart_all = false;
+        int symlink_limit = 40;
+
+        for (auto it = path_array.begin(); it != path_array.end(); ++it)
+        {
+            bool erased = false;
+            std::string tmp;
+
+            if (restart_last)
+            {
+                --it;
+                restart_last = false;
+            }
+
+            if (restart_all)
+            {
+                it = path_array.begin();
+                restart_all = false;
+            }
+
+            do
+            {
+                erased = false;
+
+                if (*it == ".")
+                {
+                    it = path_array.erase(it);
+                    erased = true;
+                }
+
+                if (*it == "..")
+                {
+                    if (it == path_array.begin())
+                    {
+                        it = path_array.erase(it);
+                    }
+                    else
+                    {
+                        it = path_array.erase(--it);
+                        it = path_array.erase(it);
+                    }
+                    erased = true;
+                }
+            } while(erased && it != path_array.end());
+
+            /* Try to build path from beginning up to this place */
+            tmp = "";
+            for (auto it2 = path_array.begin(); it2 != it; ++it2)
+            {
+                tmp += "/" + *it2;
+            }
+            tmp += "/" + *it;
+
+            D(bug("test path: %s\n", tmp.c_str()));
+            int linksize = syscall(SYS_readlinkat, AT_FDCWD, tmp.c_str(), linkbuf, 511);
+
+            if (linksize > 0)
+            {
+                linkbuf[linksize] = 0;
+                D(bug("path was a symlink pointing to: %s\n", linkbuf));
+
+                if (--symlink_limit <= 0)
+                {
+                    D(bug("limit of symbolic links exceeded. breaking\n"));
+                    break;
+                }
+
+                if (linkbuf[0] == '/')
+                {
+                    D(bug("absolute symlink, replacing everything up to this entry with it.\n"));
+                    std::list<std::string> sym = split_to_list(linkbuf, 1);
+                    sym.insert(sym.end(), std::next(it), path_array.end());
+                    path_array = sym;
+                    restart_all = true;
+                }
+                else
+                {
+                    // Remove current item (the symlink), and put new vector from symlink value in its
+                    // place
+                    std::list<std::string> sym = split_to_list(linkbuf);
+                    it = path_array.erase(it);
+                    it = path_array.insert(it, sym.begin(), sym.end());
+                    restart_last = true;
+                }
+            }
+        }
+
+        print(path_array);
+
+        for (auto it: path_array)
+            p.__path += "/" + it;
+
+        /* Check volumes */
+        int fd = openat(__TmpDirLock, ".volumes", O_RDONLY | O_DIRECTORY);
+        D(bug("volumes fd = %d\n", fd));
+
+        int len = 0;
+        int bestmatch = 0;
+
+        do {
+            struct dirent64 de, *d=&de;
+            len = syscall(SYS_getdents64, fd, &de, sizeof(de));
+            if (len == 0)
+                break;
+
+            do {
+                D(bug("len: %d\nentry: %s\n", d->d_reclen, d->d_name));
+                if ((d->d_name[0] == '.' && d->d_name[1] == 0) ||
+                    (d->d_name[0] == '.' && d->d_name[1] == '.' && d->d_name[2] == 0))
+                {
+
+                }
+                else
+                {
+                    D(bug("checking volume '%s'\n", d->d_name));
+                    int sz = readlinkat(fd, d->d_name, linkbuf, 511);
+                    linkbuf[sz] = 0;
+                    D(bug("link pointing to: %s\n", linkbuf));
+                    std::string b(linkbuf);
+
+                    if (*b.rbegin() != '/')
+                        b.push_back('/');
+
+                    if (b == p.__path.substr(0, b.length()))
+                    {
+                        if (bestmatch < b.length())
+                        {
+                            p.__volume = d->d_name;
+                            bestmatch = b.length();
+                            found_match = true;
+                        }
+                    }
+                }
+
+                len -= d->d_reclen;
+                d = (struct dirent64 *)((intptr_t)d + d->d_reclen);
+            } while (len > 0);
+        } while(true);
+
+        if (found_match)
+            p.__path = p.__path.substr(bestmatch);
+
+        close(fd);
+
+        /* Worst guess ever, assume this is a path within UNIX: assign */
+        if (!found_match)
+        {
+            p.__volume = "UNIX";
+            p.__path = path.substr(1);
+        }
+
+        D(bug("[DOS] Volume: %s, Path: %s\n", p.__volume.c_str(), p.__path.c_str()));
+    }
 
     return p;
 }
