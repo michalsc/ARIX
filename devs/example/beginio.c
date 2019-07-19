75533e80e2a68be47cdd93ee5179e421c8e67e2c
Michal Schulz <michal.schulz@tu-clausthal.de>
2019-07-19 16:26:06 +0200

    add example.device to the build

---
+++ devs/example/beginio.c
@@ -0,0 +1,7 @@
+#include <exec/devices.h>
+#include <exec/io.h>
+
+void BeginIO(struct IORequest *io)
+{
+    (void)io;
+}
---
+++ devs/example/example_init.c
@@ -0,0 +1,39 @@
+/*
+    Copyright © 2018 Michal Schulz <michal.schulz@gmx.de>
+    https://github.com/michalsc
+
+    This Source Code Form is subject to the terms of the
+    Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+    with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include <exec/types.h>
+#include <exec/libraries.h>
+#include <clib/debug_protos.h>
+
+#include <stdio.h>
+
+extern struct Library *ExampleBase;
+static void *_handle = NULL;
+
+struct Library *Open(void *h, uint32_t version)
+{
+    (void)version;
+    _handle = h;
+    return ExampleBase;
+}
+
+void *Close()
+{
+    return _handle;
+}
+
+void *Expunge()
+{
+    return NULL;
+}
+
+void *GetHandle()
+{
+    return _handle;
+}
